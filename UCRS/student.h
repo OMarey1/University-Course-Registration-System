5227916928e1adcd29f7211a12f7a14e4e81fe56
Andrew Khalifa <andrew_khalifa@aucegypt.edu>
2025-04-29 15:52:49 +0300

    Added student.h file

--- UCRS/student.h
+++ UCRS/student.h
@@ -2,11 +2,27 @@
 #define STUDENT_H
 
 #include "user.h"
+#include "Course.h"
+#include<vector>
+using namespace std;
+
 
 class Student : public User
 {
+private:
+    int studentID;
+    vector <Course*> registeredCourses;
+
 public:
     Student();
+    Student(int ID);
+    int getStudentID() const;
+    void setStudentID(int ID);
+    bool registerCourse(Course* course);
+    bool dorpCourse(Course* course);
+    vector<Course*> getCourses() const;
+
+    ~Student();
 };
 
 #endif // STUDENT_H
